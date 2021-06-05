c0d7ea63e584def2ce5320ab05a0b902a9c161a2
Daniel Corner <danielcorner7@gmail.com>
2021-06-05 03:21:36 -0400

    checkpoint

--- src/components/ScrollingOverlay.tsx
+++ src/components/ScrollingOverlay.tsx
@@ -1,7 +1,7 @@
 import { useAnimationStep } from "../store";
 import { useControl } from "react-three-gui";
 import { NUM_ANIMATION_STEPS } from "../utils/constants";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import * as THREE from "three";
 import { useMediaQuery } from "@material-ui/core";
 import { useFrame } from "@react-three/fiber";
@@ -35,41 +35,53 @@ function NumberIndicator({ num, rot }) {
   const animationStep = useAnimationStep();
   const rotX = (-rot * num + -0.4 * animationStep) / Math.PI;
   const translateY = -Math.sin(animationStep / NUM_ANIMATION_STEPS);
+  const heightIndex = num - animationStep;
+
   if (num === 0) {
     console.log("🌟🚨 ~ NumberIndicator ~ animationStep", animationStep);
     console.log("🌟🚨 ~ NumberIndicator ~ translateY", translateY);
     console.log("🌟🚨 ~ NumberIndicator ~ rotX", rotX);
+    console.log("🌟🚨 ~ NumberIndicator ~ heightIndex", heightIndex);
   }
-  const animatedRef = useRef(null as any);
+  const ref = useRef(null as any);
   // const springProps = useSpring({
   //   rotX,
   //   translateY,
   //   onFrame: ({ rotX, translateY }) => {
-  //     animatedRef.current.rotation.x =
+  //     ref.current.rotation.x =
   //       degToRad(90) * 0.14 * (num - animationStep);
-  //     // animatedRef.current.translateY = translateY;
+  //     // ref.current.translateY = translateY;
   //   },
   // });
 
+  const [x, y, z] = [
+    degToRad(90) * 0.14 * heightIndex,
+    -heightIndex * height,
+    -0.58 * heightIndex ** 2.04,
+  ];
+  console.log("🌟🚨 ~ NumberIndicator ~ y", y);
+
+  const [position, setPosition] = useState([x, y, z]);
+
   useFrame(() => {
-    animatedRef.current.rotation.x =
-      degToRad(90) * 0.14 * (num - animationStep);
+    setPosition([x, y, z]);
+    // ref.current.rotation.x = degToRad(90) * 0.14 * heightIndex;
+    // ref.current.position.y = -heightIndex * height;
+    // ref.current.position.z = -0.58 * heightIndex ** 2.04;
   });
 
   return (
     <mesh
-      ref={animatedRef}
-      {...{
-        position: [
-          0,
-          -(num - animationStep) * height,
-          -0.58 * (num - animationStep) ** 2.04,
-        ],
-        rotation: [degToRad(90) * 0.14 * (num - animationStep), 0, 0],
-        translate: [0, 10, 0],
-      }}
+      ref={ref}
+      position={position}
+      // {...{
+      //   // position: [0, -heightIndex * height, -0.58 * heightIndex ** 2.04],
+      //   position,
+      //   // rotation: [degToRad(90) * 0.14 * heightIndex, 0, 0],
+      //   // translate: [0, 10, 0],
+      // }}
     >
-      <boxBufferGeometry args={[width, height, depth]} />
+      <boxBufferGeometry args={[width, height, depth]} position={position} />
       <meshLambertMaterial color={"red"} />
     </mesh>
   );
