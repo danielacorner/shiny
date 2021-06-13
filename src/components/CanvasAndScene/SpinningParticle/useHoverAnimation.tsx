fdf1ffd286a9af4b01f638597c6b7630e990f1c0
Daniel Corner <danielcorner7@gmail.com>
2021-06-13 01:21:22 -0400

    turn off hover animation while rolling

--- src/components/CanvasAndScene/SpinningParticle/useHoverAnimation.tsx
+++ src/components/CanvasAndScene/SpinningParticle/useHoverAnimation.tsx
@@ -1,15 +1,26 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { useIsZoomed } from "../../store/store";
+import { useIsZoomed, useStore } from "../../store/store";
 
 export function useHoverAnimation() {
   const ref = useRef(null as any);
   const izZoomed = useIsZoomed();
+  const isRollingDie = useStore((s) => s.isRollingDie);
   useFrame(({ clock }) => {
     if (!izZoomed) {
       return;
     }
     if (ref.current) {
+      if (isRollingDie) {
+        ref.current.position.y = 0;
+        ref.current.position.x = 0;
+        ref.current.position.z = 0;
+
+        ref.current.rotation.x = 0;
+        ref.current.rotation.y = 0;
+        ref.current.rotation.z = 0;
+        return;
+      }
       ref.current.position.y = Math.sin(clock.getElapsedTime()) * 0.08;
       ref.current.position.x = Math.sin(clock.getElapsedTime()) * 0.05;
       ref.current.position.z = Math.sin(clock.getElapsedTime()) * 0.03;
